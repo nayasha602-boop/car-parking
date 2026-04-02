5e31d8944f62625c330763120eed3ec56d514538
nayasha602-boop <nayasha602@gmail.com>
2026-04-03 03:33:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,19 +21,19 @@ app.use(express.static(path.join(__dirname, "public")));
 // =====================
 // DATABASE CONNECTION
 // =====================
-const db = mysql.createConnection({
-    host:     process.env.DB_HOST,
-    user:     process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port:     parseInt(process.env.DB_PORT) || 3306
-});
+//const db = mysql.createConnection({
+ //   host:     process.env.DB_HOST,
+  //  user:     process.env.DB_USER,
+   // password: process.env.DB_PASS,
+   // database: process.env.DB_NAME,
+   // port:     parseInt(process.env.DB_PORT) || 3306
+//});
 
-db.connect(err => {
-    if (err) { console.error("❌ DB Error:", err); return; }
-    console.log("✅ MySQL Connected!");
-    createTables();
-});
+//db.connect(err => {
+ //   if (err) { console.error("❌ DB Error:", err); return; }
+   // console.log("✅ MySQL Connected!");
+   // createTables();
+//});
 
 // =====================
 // AUTO CREATE TABLES
