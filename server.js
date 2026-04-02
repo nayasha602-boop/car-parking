c104087c6a853afee756c7cb1ed61c72062ccf50
nayasha602-boop <nayasha602@gmail.com>
2026-04-03 03:27:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(cors({
     methods: ["GET", "POST"],
     credentials: true
 }));
+app.get("/", (req, res) => {
+    res.send("Backend is running ✅");
+});
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
